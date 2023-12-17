2b918df8acf990df60fd0376353fbbe4cbe4d1f4
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-12-17 18:34:02 +0600

    password encrypted

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   email: { type: String, require: true },
   password: { type: String, require: true },
 });
 
-// export const UserModel = mongoose.model("user", userSchema);
+// encryption password
+const encKey = process.env.ENC_KEY;
+userSchema.plugin(encrypt, {
+  secret: encKey,
+  encryptedFields: ["password"],
+});
 
+// export const UserModel = mongoose.model("user", userSchema);
 module.exports = mongoose.model("user", userSchema);
